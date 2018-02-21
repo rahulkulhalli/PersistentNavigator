fed9711ce7ab682300e4da4ba0914837e7e4de8d
rahulkulhalli <rahulkulhalli@gmail.com>
2018-02-21 14:31:27 +0530

    Reverted: Initial commit

--- app/src/main/java/com/semicolons/persitentnavigators/Utility.java
+++
@@ -1,8 +0,0 @@
-package com.semicolons.persitentnavigators;
-
-public class Utility {
-
-    public static boolean isValidString(String string) {
-        return (string != null && !string.isEmpty());
-    }
-}
